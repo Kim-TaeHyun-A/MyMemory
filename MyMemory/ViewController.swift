cee1e5fe4b43a2e3a5930c958cc3577032c681bf
Kim-TaeHyun-A <97kth01@naver.com>
2021-12-23 00:32:29 +0900

    Initial Commit

---
+++ MyMemory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by 김태현 on 2021/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
